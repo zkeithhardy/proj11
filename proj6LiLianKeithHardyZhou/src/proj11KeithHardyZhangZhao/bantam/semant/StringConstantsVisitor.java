83378607dec0ac41035a63bae57d8cd118dbfc97
TiaTalks <TiaTalks@users.noreply.github.com>
2019-02-10 22:40:01 -0500

    Add files via upload
    Initial code for the String Constants Visitor

--- proj6LiLianKeithHardyZhou/src/proj11KeithHardyZhangZhao/bantam/semant/StringConstantsVisitor.java
+++ proj6LiLianKeithHardyZhou/src/proj11KeithHardyZhangZhao/bantam/semant/StringConstantsVisitor.java
@@ -1,13 +1,58 @@
+/*
+ * File: StringConstantsVisitor.java
+ * Names: Zeb Keith-Hardy, Danqing Zhao, Tia Zhang
+ * Class: CS 461
+ * Project 11
+ * Date: February 13, 2019
+ */
+
+
+
+
 package proj11KeithHardyZhangZhao.bantam.semant;
 
-import proj11KeithHardyZhangZhao.bantam.ast.*;
+import proj11KeithHardyZhangZhao.bantam.ast.ConstStringExpr;
 import proj11KeithHardyZhangZhao.bantam.visitor.Visitor;
-
+import proj11KeithHardyZhangZhao.bantam.ast.Program;
 import java.util.Map;
+import java.util.HashMap;
 
 public class StringConstantsVisitor extends Visitor {
+    private int numStringConsts; //How many string constants have been counted so far, used for constants' map
+    private Map<String, String> stringConstMap; //Map of all string constants to a string constant identifier
 
-    public Map<String,String> getStringConstants(Program node){
+    /*
+    *
+    */
+    public StringConstantsVisitor(){
+        numStringConsts = 0;
+        stringConstMap = new HashMap<>();
+    }
+
+
+    /*
+    *
+    */
+    public Map<String, String> getStringConstants(Program ast){
+        ast.accept(this);
+        return stringConstMap;
+    }
+
+
+    /*
+    *
+    */
+    public Object visit(ConstStringExpr node) {
+        //Even if the same constant appeared multiple times, it doesn't matter if the value is updated
+        //It's still a unique number
+        //TODO double check that Dale doesn't care
+        stringConstMap.put(node.getConstant(), "StringConst_" + numStringConsts);
+        numStringConsts += 1;
         return null;
     }
 }
+
+
+
+
+
