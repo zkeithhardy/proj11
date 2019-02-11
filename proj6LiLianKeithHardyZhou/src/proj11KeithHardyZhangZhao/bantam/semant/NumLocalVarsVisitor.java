2424edc27ee5628841732128b2d3d136b6be2436
Zeb Keith-Hardy <zkeithhardy@gmail.com>
2019-02-10 20:09:35 -0500

    added visitors

---
+++ proj6LiLianKeithHardyZhou/src/proj11KeithHardyZhangZhao/bantam/semant/NumLocalVarsVisitor.java
@@ -0,0 +1,6 @@
+package proj11KeithHardyZhangZhao.bantam.semant;
+
+import proj11KeithHardyZhangZhao.bantam.visitor.Visitor;
+
+public class NumLocalVarsVisitor extends Visitor {
+}
---
+++ proj6LiLianKeithHardyZhou/src/proj11KeithHardyZhangZhao/bantam/semant/StringConstantsVisitor.java
@@ -0,0 +1,6 @@
+package proj11KeithHardyZhangZhao.bantam.semant;
+
+import proj11KeithHardyZhangZhao.bantam.visitor.Visitor;
+
+public class StringConstantsVisitor extends Visitor {
+}
