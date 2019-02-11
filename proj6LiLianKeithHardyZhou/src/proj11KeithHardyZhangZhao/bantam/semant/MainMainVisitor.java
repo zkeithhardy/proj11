41741829b1108180cd1efa564248f3680e63b192
Zeb Keith-Hardy <zkeithhardy@gmail.com>
2019-02-11 09:43:08 -0500

    added comments

--- proj6LiLianKeithHardyZhou/src/proj11KeithHardyZhangZhao/bantam/semant/MainMainVisitor.java
+++ proj6LiLianKeithHardyZhou/src/proj11KeithHardyZhangZhao/bantam/semant/MainMainVisitor.java
@@ -1,3 +1,11 @@
+/*
+ * File: NumLocalVarsVisitor.java
+ * Names: Zeb Keith-Hardy, Tia Zhang, Danqing Zhao
+ * Class: CS 461
+ * Project 11
+ * Date: February 13, 2018
+ */
+
 package proj11KeithHardyZhangZhao.bantam.semant;
 
 import proj11KeithHardyZhangZhao.bantam.ast.*;
@@ -7,6 +15,11 @@ public class MainMainVisitor extends Visitor {
     private boolean hasMainClass;
     private boolean hasMainMethod;
 
+    /**
+     * Takes in a Syntax Tree and searches the tree for a main class with a main method
+     * @param ast AST to search for main method
+     * @return boolean of whether the main method is there
+     */
     public boolean hasMain(Program ast){
         hasMainClass = false;
         hasMainMethod = false;
@@ -14,6 +27,10 @@ public class MainMainVisitor extends Visitor {
         return hasMainMethod;
     }
 
+    /**
+     * visits class node and checks if it is main class
+     * @param node the class node
+     */
     public Object visit(Class_ node){
         if(node.getName().equals("Main")){
             hasMainClass = true;
@@ -22,10 +39,20 @@ public class MainMainVisitor extends Visitor {
         return null;
     }
 
+    /**
+     * bypass fields
+     * @param node the field node
+     */
     public Object visit(Field node){
         return null;
     }
 
+    /**
+     * updated visitor so that if main method is found in main class, stops searching through
+     * the member list
+     * @param node the member list node
+     * @return
+     */
     public Object visit(MemberList node){
         for (ASTNode child : node){
             child.accept(this);
@@ -36,8 +63,14 @@ public class MainMainVisitor extends Visitor {
         return null;
     }
 
+    /**
+     * if in main class, searches for main method with correct format.
+     * @param node the method node
+     */
     public Object visit(Method node){
         if(hasMainClass){
+
+            //must be called main, have void return, and no parameters
             if(node.getName().equals("main") && node.getReturnType().equals("void")
                     && node.getFormalList().getSize() == 0){
                 hasMainMethod = true;
